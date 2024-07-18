36ab6482c921abbe452234b41c5c298e60f0281d
Aaryan Bhandari <aryanbhandari4077@gmail.com>
2024-07-18 13:32:56 +0530

    mnes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import {NavbarDefault} from './components/navbar';
+import  Navbar from './components/navbar';
 import Footer from './components/footer';
 import Loader from './components/Loader';
-import { AuthProvider } from './AuthContext'; // Assuming AuthProvider manages authentication state
+import { AuthProvider } from './AuthContext';
 
 const Home = lazy(() => import('./pages/Home'));
 const About = lazy(() => import('./pages/About'));
@@ -13,22 +13,22 @@ const Project = lazy(() => import('./pages/Project'));
 
 export default function App() {
   return (
-    // <AuthProvider> {/* Wrap your app with AuthProvider */}
-      // <BrowserRouter>
-        <NavbarDefault />
-      //   {/* <Suspense fallback={<Loader />}> {/* Fallback loader for lazy loading */}
-      //     <Routes>
-      //       <Route path='/' element={<Home />} />
-      //       <Route path='/about' element={<About />} />
-      //       <Route path='/experience' element={<Exp />} />
-      //       <Route path='/projects' element={<Projects />} />
-      //       <Route path='/project/:id' element={<Project />} />
-      //       <Route path='*' element={<NotFound />} /> {/* Handle not found routes */}
-      //     </Routes>
-      //   </Suspense>
-      //   <Footer />
-      // </BrowserRouter> */}
-    // </AuthProvider>
+    <AuthProvider> {/* Wrap your app with AuthProvider */}
+      <BrowserRouter>
+        <Navbar />
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/experience' element={<Exp />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/project/:id' element={<Project />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </Suspense>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
