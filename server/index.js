e7040526526aa98a5e20d375b22a195eeeb221af
Aaryan Bhandari <aryanbhandari4077@gmail.com>
2024-07-24 00:18:40 +0530

    djd

--- server/index.js
+++ server/index.js
@@ -2,23 +2,34 @@ const express = require('express')
 const cookie = require('cookie-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 const URI = "mongodb+srv://aryanbhandari4077:qHiT2RmS7y343QC7@cluster0.wqexvgn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const PORT = 8000
 app.use(express.json())
 app.use(cookie())
 app.use(cors({
-    origin:"*",
-    allowedHeaders:true,
-    credentials:true
+    origin: "*",
+    allowedHeaders: true,
+    credentials: true
 }));
 
-app.use('/api' , require('./routes/review'));
-app.use('/api' , require('./routes/project'));
-app.get('/' , (req,res) =>{
+app.use('/api', require('./routes/review'));
+app.use('/api', require('./routes/project'));
+app.get('/', (req, res) => {
+    const options = {
+        root: path.join(__dirname)
+    };
 
-   res.redirect('tel:7017244279')
+    const fileName = 'index.html';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            console.error('Error sending file:', err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
 })
-app.listen(PORT,() => { console.log("Running ")})
+app.listen(PORT, () => { console.log("Running ") })
 
-mongoose.connect(URI).then(() => {console.log("Connected")}).catch((err) => console.log(err));
+mongoose.connect(URI).then(() => { console.log("Connected") }).catch((err) => console.log(err));
